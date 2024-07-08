55d20989ad6124254672e1b6981b78dfbe6d9e40
Jawad Mirza <141228658+jawad026@users.noreply.github.com>
2024-07-08 10:35:17 +0500

    removed chatbot

--- web/frontend/App.jsx
+++ web/frontend/App.jsx
@@ -30,10 +30,6 @@ export default function App() {
                     label: "Campaigns",
                     destination: "/campaigns",
                   },
-                  {
-                    label: "Chatbot",
-                    destination: "/chatbot",
-                  },
                 ]}
               />
               <Routes pages={pages} />
